35e31b01c6f93b25224435cea459a4cfb6421e41
emirhandemirden <95874769+emirhandemirden@users.noreply.github.com>
2023-05-24 09:47:54 +0300

    .net-maui-simple-note-app
    That's a mobile app that you can save your notes secretly and simple. Any ads, any difficulty and any issue just open the app (right and the top) '+' > (write your note) 'save' thats it.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://github.com/emirhandemirden";
+    public string Message => "This app is written in XAML and C# with .NET MAUI. This app is made to help you write and save your notes on your phone without any hassle.";
+}
